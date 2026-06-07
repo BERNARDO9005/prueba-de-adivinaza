5812292d0b48b51b1aa0e4a1897173d0be7e7732
BERNARDO9005 <jm5566433@gmail.com>
2026-06-06 20:30:28 -0600

    Update juego de adivinanza.py

--- juego de adivinanza.py
+++ juego de adivinanza.py
@@ -4,4 +4,21 @@ import os
 from datetime import datetime
 from abc import ABC, abstractmethod
 
-
+class HighScoremanager:
+    def __int__(self, filename):
+        self._filename = filename
+        self._ensure_file_exists()
+        
+    def _ensure_file_exists(self):
+        if not os.path.exists(self._filename):
+            with open(self._filename, 'w') as f:
+                json.dump([],f)
+    
+    def load_scores(self):
+        try:
+            with open(self._filename, 'r') as f:
+                return json.load(f)
+        except (json.JSONDecodeError, IOError):
+            return []
+    
+    def save_scores(self, scores):
